b305654e84651070a366aff14a15ffc529f9f6df
ImJaeOne <dlawi0108@naver.com>
2025-05-30 02:26:49 +0900

    feat:#1 라우팅을 위한 header 구현

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,20 @@
+import { ROUTE } from "@/constants/route-constant";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <nav className="flex w-full justify-between">
+        {/* 개발 편의성을 위한 임시 nav */}
+        <Link href={ROUTE.HOME}>Home</Link>
+        <Link href={ROUTE.PROJECT("ChickMate")}>Project</Link>
+        <Link href={ROUTE.ABOUT}>About</Link>
+        <Link href={ROUTE.NOTE_POST}>Post</Link>
+        <Link href={ROUTE.NOTES}>Notes</Link>
+        <Link href={ROUTE.NOTE_DETAIL(1)}>Note Detail</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
