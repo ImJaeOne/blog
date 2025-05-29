7b25572428951f8d602245d3e2affbbe485c3315
ImJaeOne <dlawi0108@naver.com>
2025-05-30 03:34:59 +0900

    refactor:#1 slug 소문자로 변경

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
       <nav className="flex w-full justify-between">
         {/* 개발 편의성을 위한 임시 nav */}
         <Link href={ROUTE.HOME}>Home</Link>
-        <Link href={ROUTE.PROJECT("ChickMate")}>Project</Link>
+        <Link href={ROUTE.PROJECT("chickmate")}>Project</Link>
         <Link href={ROUTE.ABOUT}>About</Link>
         <Link href={ROUTE.NOTE_POST}>Post</Link>
         <Link href={ROUTE.NOTES}>Notes</Link>
